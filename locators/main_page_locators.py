7fce05e993967a04991847b0baf65e76c2ac3ecf
Irina Rogova <irinfoto@yandex.ru>
2026-06-25 18:43:17 +0300

    refactor: move all locators to locators module

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -4,16 +4,16 @@ class MainPageLocators:
     """Локаторы для главной страницы"""
     
     # Кнопки "Заказать"
-    ORDER_BUTTON_TOP = (By.XPATH, "//button[text()='Заказать']")
+    ORDER_BUTTON_TOP = (By.XPATH, "//div[contains(@class, 'Header_Nav__AGCXC')]//button[contains(@class, 'Button_Button__ra12g')]")
     ORDER_BUTTON_BOTTOM = (By.XPATH, "//button[contains(@class, 'Button_Button__ra12g') and text()='Заказать']")
     
     # Логотипы
     SCOOTER_LOGO = (By.XPATH, "//img[@alt='Scooter']")
     YANDEX_LOGO = (By.XPATH, "//img[@alt='Yandex']")
     
-    # Раздел "Вопросы о важном"
-    FAQ_SECTION = (By.CLASS_NAME, "Home_FourPart__1uthg")
-    
     # Вопросы и ответы
     QUESTION_TEMPLATE = (By.ID, "accordion__heading-{}")
     ANSWER_TEMPLATE = (By.ID, "accordion__panel-{}")
+    
+    # Баннер куки
+    COOKIE_BANNER = (By.CLASS_NAME, "App_CookieConsent__1yUIN")
--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -24,5 +24,10 @@ class OrderPageLocators:
     CONFIRM_ORDER_BUTTON = (By.XPATH, ".//button[text()='Да']")
     ORDER_SUCCESS_MESSAGE = (By.XPATH, ".//div[contains(@class, 'Order_ModalHeader__3FDaJ')]")
     
-    # Модальное окно
+    # Локаторы ошибок
+    ERROR_ELEMENTS = (By.XPATH, "//div[contains(@class, 'error') or contains(@class, 'Error')]")
+    ERROR_INPUTS = (By.XPATH, "//input[contains(@class, 'error') or contains(@class, 'Error')]")
     MODAL_WINDOW = (By.XPATH, "//div[contains(@class, 'Order_Modal')]")
+    METRO_STATION = (By.XPATH, "//div[@class='select-search__select']//button")
+    METRO_STATION_BY_TEXT = (By.XPATH, "//div[@class='select-search__select']//button[contains(text(), '{}')]")
+    DATE_DAY = (By.XPATH, "//div[contains(@class, 'react-datepicker__day') and not(contains(@class, 'react-datepicker__day--outside-month'))]")
